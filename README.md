320e8bfd7247cd3b71774db89d5b26a4d36f524d
basilry <basilry@gmail.com>
2024-03-19 20:42:24 +0900

    [update] 프로젝트명 수정, 리드미 생성, 스프링 시큐리티 기본 로그인 페이지 제거

---
+++ README.md
@@ -0,0 +1,2 @@
+# HectonProject Techblog API 프로젝트
+- - -
--- src/main/java/com/hectontechapi/config/SecurityConfig.java
+++ src/main/java/com/hectontechapi/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.hectontechapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -11,10 +12,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/healthCheck").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .httpBasic(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
